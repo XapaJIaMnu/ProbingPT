e34c083cfecb491cde6d929a9098365e7789d1e4
XapaJIaMnu <nheart@gmail.com>
2013-10-30 20:07:49 +0000

    Probing hash serialization, writing is erroneous.

---
+++ scoped_array_tests.cpp
@@ -0,0 +1,111 @@
+#include "util/file_piece.hh"
+
+#include "util/file.hh"
+#include "util/scoped.hh"
+#include "util/string_piece.hh"
+#include "util/tokenize_piece.hh"
+#include "util/murmur_hash.hh"
+#include "util/probing_hash_table.hh"
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <fstream>
+#include <iostream>
+
+#include <boost/scoped_array.hpp>
+#include <boost/functional/hash.hpp>
+#include <string>
+
+//Hash table entry
+struct Entry {
+	uint64_t key;
+	typedef unsigned char Key;
+
+	uint64_t GetKey() const {
+		return key;
+	}
+
+	void SetKey(uint64_t to) {
+		key = to;
+	}
+
+	unsigned int GetValue() const {
+		return value;
+	}
+
+	unsigned int value;
+};
+
+//Define table
+typedef util::ProbingHashTable<Entry, boost::hash<uint64_t> > Table;
+
+void serialize_table(boost::scoped_array<char> *mem, size_t size, char * filename);
+
+void readTable(char * filename, boost::scoped_array<char> *mem, size_t size);
+
+void readTable(char * filename, boost::scoped_array<char> *mem, size_t size) {
+	//Initial position of the file is the end of the file, thus we know the size
+	std::ifstream file (filename, std::ios::in|std::ios::binary);
+
+	file.read ((char *)&mem, size); // read
+
+}
+
+
+void serialize_table(boost::scoped_array<char> *mem, size_t size, char * filename){
+	std::ofstream os (filename, std::ios::binary);	
+
+	os.write((const char*)&size, sizeof(size));
+    os.write((const char*)&mem[0], size);
+    os.close();
+
+}
+
+int main() {
+
+ 	StringPiece tohash1 = StringPiece("Hash text");
+ 	int num1 = 1234;
+ 	int num2 = 3211;
+ 	StringPiece tohash2 = StringPiece("I REALLY. Like pies.");
+
+ 	std::size_t len1 = tohash1.size();
+ 	uint64_t key1 = util::MurmurHashNative(&tohash1, len1);
+ 	std::size_t len2 = tohash1.size();
+ 	uint64_t key2 = util::MurmurHashNative(&tohash2, len2);
+
+	//Make entries.
+	Entry entry1, entry2;
+	entry1.key = key1;
+	entry1.value = num1;
+	entry2.key = key2;
+	entry2.value = num2;
+
+	//Init the table
+	size_t size = Table::Size(3, 1.2);
+	boost::scoped_array<char> mem(new char[size]);
+	Table table(mem.get(), size);
+
+	table.Insert(entry1);
+	table.Insert(entry2);
+
+	const Entry * find_num;
+	table.Find(key1, find_num);
+  	std::cout << "Num 1 is " << find_num -> GetValue() << std::endl;
+
+  	//Serialize to disk test
+  	serialize_table(&mem, size, "hashtable.dat");
+
+  	boost::scoped_array<char> read(new char[size]);
+
+  	readTable("hashtable.dat", &read, size);
+  	std::cout << "Here! " << std::endl;
+  	Table table2(read.get(), size);
+  	const Entry* find_num2;
+  	table2.Find(key1, find_num2);
+  	std::cout << "Num 1 is " << find_num2 -> GetValue() << std::endl;
+
+
+  	return 0;
+}

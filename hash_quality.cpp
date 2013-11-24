b1a3ee231e1dfb645b76cd9ad457cf8a50a48e79
Nikolay Bogoychev <nheart@gmail.com>
2013-11-24 15:23:33 +0000

    Add missing ;

--- hash_quality.cpp
+++ hash_quality.cpp
@@ -96,5 +96,5 @@ int main(int argc, char* argv[]){
 		}
 	}
 
-	std::cout << "Entries: " << lines << " Collisions: " << clashes << std::endl
+	std::cout << "Entries: " << lines << " Collisions: " << clashes << std::endl;
 }
